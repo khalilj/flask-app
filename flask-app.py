054cf693a27cff1d905dc66df2a415634c68bea8
Khalil Jiries <khalil.jiries@philips.com>
2022-11-11 19:35:21 +0200

    Set default port to 80

--- flask-app.py
+++ flask-app.py
@@ -2,6 +2,6 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get('FLASK_PORT', 5000))
+    port = int(os.environ.get('FLASK_PORT', 80))
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
     app.run(debug=True, host=host, port=port)
